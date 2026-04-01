22582ccadf2bca8d9923405c42d06ed39e41f40d
Tate Maguire <tatemaguir@gmail.com>
2026-04-01 10:09:13 -0700

    drawing vectors

--- asg0.js
+++ asg0.js
@@ -1,3 +1,7 @@
+// Globals
+var ctx;
+var canvas_center;
+
 // DrawTriangle.js (c) 2012 matsuda
 function main() {  
   // Retrieve <canvas> element
@@ -8,9 +12,40 @@ function main() {
   } 
 
   // Get the rendering context for 2DCG
-  var ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  // Set canvas center
+  canvas_center = new Vector3(
+    [Math.floor(canvas.width / 2), 
+     Math.floor(canvas.height / 2),
+     0]
+  );
+
+  // Draw background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 400, 400);
+
+  // Draw a red vector
+  var v1 = new Vector3([1, 1, 0]);
+  drawVector(v1, "red");
+  console.log(v1);
+
+}
+
+// Draw vector v on canvas from center, with specified color
+function drawVector(v, color) {
+  ctx.strokeStyle = color;
+
+  var start = canvas_center.elements;
+  var offset = v.elements;
+
+  // Scale *20, reflect on y axis
+  offset[0] *= 20;
+  offset[1] *= -20;
+  console.log(offset);
+
+  ctx.beginPath();
+  ctx.moveTo(start[0], start[1]);
+  ctx.lineTo(start[0] + offset[0], start[1] + offset[1]);
+  ctx.stroke();
 }
